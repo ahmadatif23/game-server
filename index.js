1bf35e4227f0d70607a998f0ac31bd1cb97b03f2
ahmadsols <145330320+ahmadsols@users.noreply.github.com>
2025-09-03 13:37:08 +0800

    use env port

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("🚀 Socket.IO server running at http://localhost:3001");
-});
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => console.log(`🚀 Running on ${PORT}`));
